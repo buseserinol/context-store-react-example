71f22b583f74404caee052581579cf116514824b
buse-serinol <buserinoll@gmail.com>
2024-02-15 23:46:18 +0300

    card components is created

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,16 @@
 import { useContext } from "react";
 import { ProductContext } from "../context/productContext";
 import Loader from "../components/Loader";
+import Card from "../components/Card";
 const HomePage = () => {
   //abone olma işlemi
   const { products } = useContext(ProductContext);
   console.log(products);
   return (
-    <div>
+    <div className="container d-flex flex-wrap justify-content-center gap-3 justify-content-md-between gap-md-4 mt-3 p-4">
       {!products && <Loader />}
-      {products?.map((urun) => (
-        <h1>Kart</h1>
+      {products?.map((product) => (
+        <Card key={product.id} product={product} />
       ))}
     </div>
   );
